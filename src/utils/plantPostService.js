ace118231f93639dcf5796d89605254d6560b399
Thomas Strub <thomstrub@gmail.com>
2021-01-19 11:50:01 -0800

    adds plantPostService in utils and PlantPostPage for form

---
+++ src/utils/plantPostService.js
@@ -0,0 +1,23 @@
+import tokenService from './tokenService';
+
+const BASE_URL = '/api/plantswap'
+
+export function create(post){
+    return fetch(BASE_URL, {
+        method: 'POST',
+        body: post, // our info from the form
+        headers: {
+            'Authorization': 'Bearer ' + tokenService.getToken()
+        }
+    }).then(res => res.json())
+
+}
+
+export function getAll() {
+    return fetch(BASE_URL, {
+      headers: {
+        'Authorization': 'Bearer ' + tokenService.getToken()
+      }
+    })
+    .then(res => res.json());
+  }
