1cfbbae9c5f0be171c52bdab26b6136841b9ae38
Thomas Strub <thomstrub@gmail.com>
2021-01-21 14:02:57 -0800

    completes form component that correctly enters information in MongoDB

--- controllers/plantswap.js
+++ controllers/plantswap.js
@@ -1,19 +1,21 @@
 const PlantPost = require('../models/plantPost');
+const Plant = require('../models/plant');
 const { v4: uuidv4 } = require('uuid');
 const S3 = require('aws-sdk/clients/s3');
 const s3 = new S3(); // initialize the construcotr
 const fetch = require('node-fetch');
+const { getNodeText } = require('@testing-library/react');
 
 module.exports = {
     create,
     index
 }
 
-async function trefleCall(data){
+async function trefleCall(plant){
         // try{
         //     //API Call
         const proxyurl = "https://cors-anywhere.herokuapp.com/";
-        const treflePlantUrl = `https://trefle.io/api/v1/plants/${data}?token=nGl9aJhLyHSPDXgy_7THrf3UycmVNDpcU4kvluaWwZQ`;
+        const treflePlantUrl = `https://trefle.io/api/v1/plants/${plant}?token=nGl9aJhLyHSPDXgy_7THrf3UycmVNDpcU4kvluaWwZQ`;
         // await fetch(proxyurl + treflePlantUrl)
         //         .then((res) => res.json())
         //         .then((data) => {
@@ -31,37 +33,55 @@ async function trefleCall(data){
         // } catch(err){
         //     console.log(err)
         // }
-        (async () => {
-            console.log(data, "<------- data from controller")
+        let plantData = {};
+        
+            console.log(plant, "<------- data from controller")
             const response = await fetch(treflePlantUrl);
             const json = await response.json();
-            console.log(json.data.main_species, "json from controller - main species");
-          })();
-    
+            console.log(json, "json from controller");
+            // let selectedPlantData = json.data.main_species
+            // console.log(selectedPlantData, "json.data.main_species")
+            // selectedPlantData = selectedPlantData.data.main_species
+            try{
+                plantData={
+                    commonName: (json.data.main_species.common_name ? json.data.main_species.common_name: "nothing"),
+                    genus: json.data.main_species.genus,
+                    species: json.data.main_species.scientific_name,
+                    description: `From the family ${json.data.main_species.family} ${json.data.main_species.family_common_name ? json.data.main_species.family_common_name : '' }. Observed as a native species from ${json.data.main_species.observations}.`,
+                    photoUrl: json.data.main_species.image_url
+            }
+            console.log(plantData);
+            } catch(err){
+                console.log(err)
+                res.json({data: err})
+            }
+            
+          
+    return(plantData);
 }
 
 
 // We have to use AWS and multer again for this
-function create(req, res){
+async function create(req, res){
     console.log(req.file, req.body, 'this is create method', req.user)
-
-    const plantData = trefleCall(req.body.plant);
+    const plantData = await trefleCall(req.body.plant);
+    const plant = await Plant.create(plantData);
     try {
 
         const filePath = `${uuidv4()}/${req.file.originalname}`
         const params = {Bucket: 'travelgo', Key: filePath, Body: req.file.buffer};
         s3.upload(params, async function(err, data){
            
-
+            
           
             // data.Location is the address where our image is stored on aws
             const post = await PlantPost.create({description: req.body.description, quantity: req.body.quantity,
             dateCollected: req.body.dateCollected, user: req.user, photoUrl: data.Location,
-            isSeed: req.body.isSeed, isRootstock: req.body.isRootstock, forSale: req.body.forSale, plant: plantData
+            isSeed: req.body.isSeed, isRootstock: req.body.isRootstock, forSale: req.body.forSale, plant: plant._id
             });
-                console.log(selectedPlantData, "Selected Plant Data")
-
-            const populatedUserPost = await post.populate('user').execPopulate();
+                
+            const plantPopulatedPost = await post.populate('plant').execPopulate();
+            const populatedUserPost = await plantPopulatedPost.populate('user').execPopulate();
             res.status(201).json({post: populatedUserPost})
         })
 
